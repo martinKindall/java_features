8e8f24e48128127b218faf9b6a746833d02dff4e
Martin Cornejo <mart256@gmail.com>
2024-03-08 22:30:56 +0100

    Added validation for records

--- src/main/java/org/data_oriented_programming/models/FullTime.java
+++ src/main/java/org/data_oriented_programming/models/FullTime.java
@@ -1,11 +1,17 @@
 package org.data_oriented_programming.models;
 
+import java.util.Objects;
+
 public record FullTime(
         String nombre,
         boolean esContratoIndefinido,
         long salarioBruto,
         String previsionSalud
 ) implements Empleado {
+
+    public FullTime {
+        Objects.requireNonNull(nombre);
+    }
 
     public int horasSemanales() {
         return 40;
--- src/main/java/org/data_oriented_programming/models/PartTime.java
+++ src/main/java/org/data_oriented_programming/models/PartTime.java
@@ -1,9 +1,20 @@
 package org.data_oriented_programming.models;
 
+import java.util.Objects;
+
 public record PartTime(
         String nombre,
         int horasSemanales,
         boolean esContratoIndefinido,
         long salarioBruto
 
-) implements Empleado {}
+) implements Empleado {
+
+    public PartTime {
+        Objects.requireNonNull(nombre);
+
+        if (horasSemanales >= 40 || horasSemanales < 25)
+            throw new IllegalArgumentException("horas semanales no validas para Part time");
+    }
+
+}
